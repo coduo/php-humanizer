9be57709a0e83714d8bd29d55e3c437a57dbeb6c
Daniel Ribeiro <drgomesp@gmail.com>
2015-10-31 23:57:39 +0400

    Added support for multiple character separators at string humanizer

--- src/Coduo/PHPHumanizer/String.php
+++ src/Coduo/PHPHumanizer/String.php
@@ -7,9 +7,9 @@ use Coduo\PHPHumanizer\String\Truncate;
 
 class String
 {
-    public static function humanize($text, $capitalize = true)
+    public static function humanize($text, $capitalize = true, $separator = Humanize::SEPARATOR_UNDERSCORE)
     {
-        return (string) new Humanize($text, $capitalize);
+        return (string) new Humanize($text, $capitalize, $separator);
     }
 
     public static function truncate($text, $charactersCount, $append = '')
