5355710f603fd24c1b9f1f96a92c2869461ffd0d
davidavz <d4vid@live.com>
2016-05-17 15:44:29 +0200

    add fr strategy

---
+++ src/Coduo/PHPHumanizer/Resources/Ordinal/FrStrategy.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Coduo\PHPHumanizer\Resources\Ordinal;
+
+use Coduo\PHPHumanizer\Number\Ordinal\StrategyInterface;
+
+final class FrStrategy implements StrategyInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function isPrefix()
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function ordinalIndicator($number)
+    {
+        if ($number == 1) {
+            return 'er';
+        }
+
+        return 'ème';
+    }
+}
