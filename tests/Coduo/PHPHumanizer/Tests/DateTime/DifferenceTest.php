77e2f496eb65fb3cdfabacf25c54c6f0cbf333e6
Chun-Sheng, Li <peter279k@gmail.com>
2021-03-12 23:19:05 +0800

    Improve assertions (#126)

--- tests/Coduo/PHPHumanizer/Tests/DateTime/DifferenceTest.php
+++ tests/Coduo/PHPHumanizer/Tests/DateTime/DifferenceTest.php
@@ -43,7 +43,7 @@ final class DifferenceTest extends TestCase
         $diff = new Difference(new \DateTime('2014-04-26 13:00:00'), new \DateTime('2014-04-26 11:00:00'));
         $this->assertInstanceOf(Hour::class, $diff->getUnit());
         $this->assertSame(2, $diff->getQuantity());
-        $this->assertSame(true, $diff->isPast());
+        $this->assertTrue($diff->isPast());
     }
 
     public function test_calculate_diff_between_present_and_future_date_in_hours() : void
@@ -51,7 +51,7 @@ final class DifferenceTest extends TestCase
         $diff = new Difference(new \DateTime('2014-04-26 13:00:00'), new \DateTime('2014-04-26 16:00:00'));
         $this->assertInstanceOf(Hour::class, $diff->getUnit());
         $this->assertSame(3, $diff->getQuantity());
-        $this->assertSame(false, $diff->isPast());
+        $this->assertFalse($diff->isPast());
     }
 
     public function test_calculate_diff_between_present_and_past_date_in_days() : void
@@ -59,7 +59,7 @@ final class DifferenceTest extends TestCase
         $diff = new Difference(new \DateTime('2014-04-10'), new \DateTime('2014-04-09'));
         $this->assertInstanceOf(Day::class, $diff->getUnit());
         $this->assertSame(1, $diff->getQuantity());
-        $this->assertSame(true, $diff->isPast());
+        $this->assertTrue($diff->isPast());
     }
 
     public function test_calculate_diff_between_present_and_future_date_in_days() : void
@@ -67,7 +67,7 @@ final class DifferenceTest extends TestCase
         $diff = new Difference(new \DateTime('2014-04-10'), new \DateTime('2014-04-11'));
         $this->assertInstanceOf(Day::class, $diff->getUnit());
         $this->assertSame(1, $diff->getQuantity());
-        $this->assertSame(false, $diff->isPast());
+        $this->assertFalse($diff->isPast());
     }
 
     public function test_calculate_diff_between_present_and_past_date_in_weeks() : void
@@ -75,7 +75,7 @@ final class DifferenceTest extends TestCase
         $diff = new Difference(new \DateTime('2014-04-15'), new \DateTime('2014-04-01'));
         $this->assertInstanceOf(Week::class, $diff->getUnit());
         $this->assertSame(2, $diff->getQuantity());
-        $this->assertSame(true, $diff->isPast());
+        $this->assertTrue($diff->isPast());
     }
 
     public function test_calculate_diff_between_present_and_future_date_in_weeks() : void
@@ -83,7 +83,7 @@ final class DifferenceTest extends TestCase
         $diff = new Difference(new \DateTime('2014-04-01'), new \DateTime('2014-04-15'));
         $this->assertInstanceOf(Week::class, $diff->getUnit());
         $this->assertSame(2, $diff->getQuantity());
-        $this->assertSame(false, $diff->isPast());
+        $this->assertFalse($diff->isPast());
     }
 
     public function test_calculate_diff_between_present_and_past_date_in_months() : void
@@ -91,7 +91,7 @@ final class DifferenceTest extends TestCase
         $diff = new Difference(new \DateTime('2014-04-01'), new \DateTime('2014-03-01'));
         $this->assertInstanceOf(Month::class, $diff->getUnit());
         $this->assertSame(1, $diff->getQuantity());
-        $this->assertSame(true, $diff->isPast());
+        $this->assertTrue($diff->isPast());
     }
 
     public function test_calculate_diff_between_present_and_future_date_in_months() : void
@@ -99,7 +99,7 @@ final class DifferenceTest extends TestCase
         $diff = new Difference(new \DateTime('2014-04-01'), new \DateTime('2014-05-01'));
         $this->assertInstanceOf(Month::class, $diff->getUnit());
         $this->assertSame(1, $diff->getQuantity());
-        $this->assertSame(false, $diff->isPast());
+        $this->assertFalse($diff->isPast());
     }
 
     public function test_calculate_diff_between_present_and_past_date_in_years() : void
@@ -107,7 +107,7 @@ final class DifferenceTest extends TestCase
         $diff = new Difference(new \DateTime('2014-01-01'), new \DateTime('2012-01-01'));
         $this->assertInstanceOf(Year::class, $diff->getUnit());
         $this->assertSame(2, $diff->getQuantity());
-        $this->assertSame(true, $diff->isPast());
+        $this->assertTrue($diff->isPast());
     }
 
     public function test_calculate_diff_between_present_and_future_date_in_years() : void
@@ -115,6 +115,6 @@ final class DifferenceTest extends TestCase
         $diff = new Difference(new \DateTime('2014-01-01'), new \DateTime('2015-01-01'));
         $this->assertInstanceOf(Year::class, $diff->getUnit());
         $this->assertSame(1, $diff->getQuantity());
-        $this->assertSame(false, $diff->isPast());
+        $this->assertFalse($diff->isPast());
     }
 }
