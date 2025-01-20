fb621fda3e73428af8bba55f697c51daf002580b
Norbert Orzechowicz <contact@norbert.tech>
2025-01-19 22:13:07 -0600

    Updated project to PHP 8.4

--- src/Coduo/PHPHumanizer/Aeon/Calendar/Formatter.php
+++ src/Coduo/PHPHumanizer/Aeon/Calendar/Formatter.php
@@ -26,6 +26,10 @@ final class Formatter
 
     public function timeUnit(Unit $unit, string $locale = 'en') : string
     {
+        if (!\class_exists('\Aeon\Calendar\Gregorian\Calendar')) {
+            throw new \RuntimeException('Please add "aeon-php/calendar": ^1.0 to composer.json first');
+        }
+
         $parts = [];
 
         foreach ((new UnitCompound($unit))->components() as $component) {
